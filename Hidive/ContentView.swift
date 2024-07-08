30f57d20cde582d920c39a0a0ad77444a4f7fd03
Auties00 <alautiero@gmail.com>
2024-07-08 16:03:37 +0200

    Initial Commit

---
+++ Hidive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hidive
+//
+//  Created by Alessandro Autiero on 08/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hidive/HidiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HidiveApp.swift
+//  Hidive
+//
+//  Created by Alessandro Autiero on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HidiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
