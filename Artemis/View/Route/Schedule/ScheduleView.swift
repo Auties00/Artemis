9f2cc584f78079c685f892d2c2e1ba5c180c187c
Auties00 <alautiero@gmail.com>
2024-08-15 15:29:57 +0200

    Initial Commit

---
+++ Artemis/View/Route/Schedule/ScheduleView.swift
@@ -0,0 +1,84 @@
+//
+//  HomeView.swift
+//  Hidive
+//
+//  Created by Alessandro Autiero on 08/07/24.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    private static let headerId: String = "scheduleHeader"
+    
+    @Environment(AccountController.self)
+    private var accountController: AccountController
+    
+    @Environment(ScheduleController.self)
+    private var scheduleController: ScheduleController
+    
+    @Environment(RouterController.self)
+    private var routerController: RouterController
+    
+    @Environment(\.colorScheme)
+    private var colorScheme: ColorScheme
+    
+    @State
+    private var showSheet = false
+    
+    var body: some View {
+        TabNavigationView(title: "Schedule") {
+            GeometryReader { geometry in
+                ScrollViewReader { scrollProxy in
+                    List {
+                        switch(scheduleController.data) {
+                        case .success(data: let data):
+                            loadedBody(data: data, geometry: geometry)
+                        case .empty, .loading:
+                            ExpandedView(geometry: geometry) {
+                                LoadingView()
+                            }
+                        case .error(error: let error):
+                            ExpandedView(geometry: geometry) {
+                                ErrorView(error: error)
+                            }
+                        }
+                    }
+                    .onAppear {
+                        routerController.pathHandler = {
+                            withAnimation {
+                                scrollProxy.scrollTo(ScheduleView.headerId, anchor: .center)
+                            }
+                            return true
+                        }
+                    }
+                }
+            }
+            .refreshable {
+                if(accountController.profile.value == nil) {
+                    await accountController.login()
+                }
+                
+                await scheduleController.loadData()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func loadedBody(data: [ScheduleEntry], geometry: GeometryProxy) -> some View {
+        if(data.isEmpty) {
+            ExpandedView(geometry: geometry) {
+                ErrorView(
+                    title: "No releases",
+                    description: "No releases are scheduled for this week",
+                    systemImage: "film.stack.fill"
+                )
+            }
+        }else {
+            ForEach(data) { scheduleEntry in
+                ScheduleCardView(headerId: scheduleEntry == data.first ? ScheduleView.headerId : nil, entry: scheduleEntry) { addToWatchlistItem in
+                    routerController.addToWatchlistItem = addToWatchlistItem
+                }
+            }
+        }
+    }
+}
